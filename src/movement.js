27aeef0c8027fb552aebdde7799d032d4df1dcaf
Elliott David <93677169+electricsinge@users.noreply.github.com>
2024-03-07 11:45:00 -0600

    Added Movement

--- src/movement.js
+++ src/movement.js
@@ -1,1 +1,13 @@
 let inBattle;
+
+eventTarget.addEventListener("keydown", (event) => {
+  switch(event.keyCode){
+    case 38:
+      playerWorld.x = playerWorld.speed;
+    case 40:
+      playerWorld.x = (playerWorld.speed * -1);
+    case 37:
+      playerWorld.y = (playerWorld.speed * -1);
+    case 39:
+      playerWorld.y = (playerWorld.speed);
+}});
