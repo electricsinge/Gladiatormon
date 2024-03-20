2788c45eccf6ba5e505a8eefdecfeb1e6641b597
Elliott David <93677169+electricsinge@users.noreply.github.com>
2024-03-20 10:05:04 -0500

    Update movement.js

--- src/movement.js
+++ src/movement.js
@@ -133,7 +133,7 @@ movingBackground.y = 300;
 
 function move(){
 
-  document.getElementById("coordinate").innerText = playerWorld.location;
+  //document.getElementById("coordinate").innerText = playerWorld.location;
 
   let pixelLocation = [movingBackground.x + (gameContainer.getBoundingClientRect().width/2), movingBackground.y + (gameContainer.getBoundingClientRect().height/2)]
 
